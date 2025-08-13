c2e7817ccc0f02150f297a95f202272dc0184be2
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-08-13 22:50:15 +0100

    pipe spawn pos calculated on server

--- Assets/FlappyBirdLevel.cs
+++ Assets/FlappyBirdLevel.cs
@@ -19,14 +19,15 @@ public class FlappyBirdLevel : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     public void SpawnPipesServerRpc()
     {
-        SpawnPipesClientRpc();
+        float height = Random.Range(minHeight, maxHeight);
+        Vector3 spawnPos = new Vector3(spawnPoint.position.x, spawnPoint.position.y + height, spawnPoint.position.z);
+        SpawnPipesClientRpc(spawnPos);
     }
 
     [ClientRpc]
-    void SpawnPipesClientRpc()
+    void SpawnPipesClientRpc(Vector3 spawnPos)
     {
-        float height = Random.Range(minHeight, maxHeight);
-        Vector3 spawnPos = new Vector3(spawnPoint.position.x, spawnPoint.position.y + height, spawnPoint.position.z);
+
         GameObject pipe = Instantiate(pipeSet, spawnPos, pipeSet.transform.rotation);
         pipe.transform.SetParent(transform);
         spawnedPipes.Add(pipe);
