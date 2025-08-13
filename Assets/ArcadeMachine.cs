07818304418f6ac477862e8016a8f32c7c34dec8
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-08-13 22:07:01 +0100

    added screen turning on and off when players are close

--- Assets/ArcadeMachine.cs
+++ Assets/ArcadeMachine.cs
@@ -1,11 +1,36 @@
+using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ArcadeMachine : MonoBehaviour
+public class ArcadeMachine : NetworkBehaviour
 {
     public ArcadeGame arcadeGame;
+    public GameObject screen;
+
+    public List<GameObject> nearPlayers = new List<GameObject>();
 
     public void StartGame()
     {
         arcadeGame.Begin();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void TurnOnServerRpc()
+    {
+        TurnOnClientRpc();
+    }
+
+    [ClientRpc]
+    void TurnOnClientRpc()
+    {
+        screen.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (nearPlayers.Count == 0)
+        {
+            screen.SetActive(false);    
+        }
     }
 }
--- Assets/Interact.cs
+++ Assets/Interact.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class Interact : MonoBehaviour
+public class Interact : NetworkBehaviour
 {
 
     public float range = 5f;
@@ -16,6 +17,8 @@ public class Interact : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner) return;
+
         if (Input.GetKeyDown(interactKey))
         {
             if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, range))
@@ -29,5 +32,37 @@ public class Interact : MonoBehaviour
             }
         }
 
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ArcadeMachine"))
+        {
+            ArcadeMachine machine = other.GetComponent<ArcadeMachine>();
+            if (machine)
+            {
+                machine.TurnOnServerRpc();
+                if (!machine.nearPlayers.Contains(gameObject))
+                {
+                    machine.nearPlayers.Add(gameObject);
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ArcadeMachine"))
+        {
+            ArcadeMachine machine = other.GetComponent<ArcadeMachine>();
+            if (machine)
+            {
+                if (machine.nearPlayers.Contains(gameObject))
+                {
+                    machine.nearPlayers.Remove(gameObject);
+                }
+            }
+        }
     }
 }
