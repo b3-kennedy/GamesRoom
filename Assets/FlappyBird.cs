6f8ec46369dbd7d660976eaba6412311b557cb28
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-08-14 12:48:48 +0100

    put reset logic on server

--- Assets/FlappyBird.cs
+++ Assets/FlappyBird.cs
@@ -48,12 +48,13 @@ public class FlappyBird : ArcadeGame
     public override void Reset()
     {
 
-        bird.transform.localPosition = new Vector3(0f, 0f, 1.59f);
-        bird.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
-        scoreText.text = "SCORE: 0";
-        level.ClearPipes();
+
         if (IsServer)
         {
+            bird.transform.localPosition = new Vector3(0f, 0f, 1.59f);
+            bird.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            scoreText.text = "SCORE: 0";
+            level.ClearPipes();
             score.Value = 0;
             netGameState.Value = GameState.MAIN_MENU;
         }
