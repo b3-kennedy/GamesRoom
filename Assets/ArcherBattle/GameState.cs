1fae0eb2f32d27398489ee0284e8e5d6acea31dd
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-09-01 17:01:18 +0100

    syncing camera

--- Assets/ArcherBattle/GameState.cs
+++ Assets/ArcherBattle/GameState.cs
@@ -106,34 +106,30 @@ namespace Assets.ArcherBattle
         public void OnTurnEndServerRpc()
         {
             isLeftPlayerTurn.Value = !isLeftPlayerTurn.Value;
+            Vector3 leftPos = leftPlayerObject.transform.position;
+            Vector3 rightPos = rightPlayerObject.transform.position;
             if (isLeftPlayerTurn.Value)
             {
                 archerBattleGame.leftPlayer.GetComponent<ArcheryPlayer>().isTurn.Value = true;
                 archerBattleGame.rightPlayer.GetComponent<ArcheryPlayer>().isTurn.Value = false;
+                cam.transform.position = new Vector3(leftPos.x, leftPos.y, cam.transform.position.z);
             }
             else
             {
                 archerBattleGame.leftPlayer.GetComponent<ArcheryPlayer>().isTurn.Value = false;
                 archerBattleGame.rightPlayer.GetComponent<ArcheryPlayer>().isTurn.Value = true;
+                cam.transform.position = new Vector3(rightPos.x, rightPos.y, cam.transform.position.z);
+
             }
 
 
-            MoveCameraClientRpc();
+            MoveCameraClientRpc(cam.transform.position);
         }
 
         [ClientRpc]
-        void MoveCameraClientRpc()
+        void MoveCameraClientRpc(Vector3 pos)
         {
-            Vector3 leftPos = leftPlayerObject.transform.position;
-            Vector3 rightPos = rightPlayerObject.transform.position;
-            if (isLeftPlayerTurn.Value)
-            {
-                cam.transform.position = new Vector3(leftPos.x, leftPos.y, cam.transform.position.z);
-            }
-            else
-            {
-                cam.transform.position = new Vector3(rightPos.x, rightPos.y, cam.transform.position.z);
-            }
+            cam.transform.position = pos;
         }
 
 
