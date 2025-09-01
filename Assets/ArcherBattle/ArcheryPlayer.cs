5255e9baed021321b7e038d7fb92728ec21b9d65
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-09-01 17:39:55 +0100

    changed direction

--- Assets/ArcherBattle/ArcheryPlayer.cs
+++ Assets/ArcherBattle/ArcheryPlayer.cs
@@ -65,7 +65,8 @@ namespace Assets.ArcherBattle
             }
             else if (Input.GetKeyUp(KeyCode.Space))
             {
-                game.gameState.LaunchArrowServerRpc(arrowSpawn.position, -rotater.eulerAngles.normalized, charge);
+                Vector3 direction = rotater.right;
+                game.gameState.LaunchArrowServerRpc(arrowSpawn.position, direction, charge);
                 charge = 0;
             }
 
