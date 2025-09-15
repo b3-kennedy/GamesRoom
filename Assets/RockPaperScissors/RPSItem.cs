53c7741663bd0452f5657c5b8138665de31da6c3
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-09-15 16:47:29 +0100

    fixing

--- Assets/RockPaperScissors/RPSItem.cs
+++ Assets/RockPaperScissors/RPSItem.cs
@@ -5,11 +5,10 @@ public class RPSItem : MonoBehaviour
 {
 
     public bool isSelected;
-    GameObject outline;
+    public GameObject outline;
 
     void Start()
     {
-        outline = transform.GetChild(1).gameObject;
     }
 
     public void ChangeSelectedValue(bool value)
