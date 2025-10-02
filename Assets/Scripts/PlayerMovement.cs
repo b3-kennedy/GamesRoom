2c9994723079ca904ec0a3bab4c9642935caee9a
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-10-02 13:49:17 +0100

    fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -52,7 +52,8 @@ public class PlayerMovement : NetworkBehaviour
 
     void Update()
     {
-        
+
+        GetUp();
 
         if (!IsOwner) return;
 
@@ -91,7 +92,7 @@ public class PlayerMovement : NetworkBehaviour
             }
         }
 
-        GetUp();
+        
 
     }
     
