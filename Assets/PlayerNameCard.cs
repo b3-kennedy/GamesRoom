89cbb665d9d93510d02f9d4fee1c33ce84b5c30c
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-10-09 15:55:51 +0100

    fix

--- Assets/PlayerNameCard.cs
+++ Assets/PlayerNameCard.cs
@@ -15,9 +15,29 @@ public class PlayerNameCard : NetworkBehaviour
 
     public override void OnNetworkSpawn()
     {
-        player = transform.root.gameObject;
-        cam = player.GetComponent<PlayerLook>().normalCamera.gameObject;
-        nameTMP.text = player.GetComponent<SteamPlayer>().playerName;
+        var localPlayer = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+        if (localPlayer != null)
+        {
+            cam = localPlayer.GetComponent<PlayerLook>().normalCamera.gameObject;
+        }
+
+        if (IsServer)
+        {
+            player = transform.root.gameObject;
+            nameTMP.text = player.GetComponent<SteamPlayer>().playerName;
+            SetPlayerNameClientRpc(player.GetComponent<NetworkObject>().NetworkObjectId, nameTMP.text);
+        }
+        
+    }
+    
+    [ClientRpc]
+    void SetPlayerNameClientRpc(ulong playerNetID, string name)
+    {
+        if(NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(playerNetID, out var player))
+        {
+            PlayerNameCard card = player.transform.GetChild(4).GetComponent<PlayerNameCard>();
+            card.nameTMP.text = name;
+        }
     }
 
     // Update is called once per frame
