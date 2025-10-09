eef558f18a929f0d8af2eab20184a0be40a2a8a5
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-10-09 15:35:39 +0100

    adding namecards

---
+++ Assets/PlayerNameCard.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Unity.Netcode;
+using TMPro;
+
+public class PlayerNameCard : MonoBehaviour
+{
+    public TextMeshProUGUI nameTMP;
+    GameObject player;
+    GameObject cam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject().gameObject;
+        cam = player.GetComponent<PlayerLook>().normalCamera.gameObject;
+        nameTMP.text = player.GetComponent<SteamPlayer>().playerName; 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (cam == null) return;
+
+        // Make the name card face the camera
+        transform.LookAt(cam.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
